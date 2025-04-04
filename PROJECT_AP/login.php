acd01541d646881403f03016bc45001a6220c6f5
Sanchitgarg639 <sanchitgarg639@gmail.com>
2025-04-04 10:44:04 +0530

    Update login.php

--- PROJECT_AP/login.php
+++ PROJECT_AP/login.php
@@ -49,7 +49,7 @@
         <!-- Bootstrap CSS -->
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     	<link href="https://kit-pro.fontawesome.com/releases/v5.15.3/css/pro.min.css" rel="stylesheet">
-        <link rel="stylesheet" href="css/signup.css">
+        <link rel="stylesheet" href="css/signups.css">
 
         <title>JobSeek | Login.</title>
     </head>
