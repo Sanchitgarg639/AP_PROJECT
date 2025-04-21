ee716e5db2d32cdd1850b7794fcb9b862ea414e6
Sanchitgarg639 <sanchitgarg639@gmail.com>
2025-04-22 04:05:40 +0530

    Delete PROJECT_AP directory

--- PROJECT_AP/logout.php
+++
@@ -1,8 +0,0 @@
-<?php
-    session_start();
-    session_destroy();
-    session_unset();
-    setcookie('usernumcookie',$userphone,time()-(86400*31));
-    header("Location: login.php?logout=true");
-
-?>
