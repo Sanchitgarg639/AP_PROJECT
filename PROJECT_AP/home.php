62a3096a3883850b91a949706f4ae4cf6e0a3d3b
Sanchitgarg639 <sanchitgarg639@gmail.com>
2025-04-21 19:17:32 +0530

    Update home.php

--- PROJECT_AP/home.php
+++ PROJECT_AP/home.php
@@ -14,6 +14,7 @@
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
     <link href="https://kit-pro.fontawesome.com/releases/v5.15.3/css/pro.min.css" rel="stylesheet">
     <link rel="stylesheet" href="css/home.css">
+    <link rel="stylesheet" href="css/_footerc.css">
 </head>
 <body>
     <div>
@@ -74,11 +75,7 @@
         </div>
     </section>
 
-    <footer class="footer">
-        <div class="container text-center">
-            <p>&copy; 2025 JobSeek. All Rights Reserved.</p>
-        </div>
-    </footer>
+    <?php include 'partial/_footer.php' ?>
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
